e69ff933e79f15fc49c8f1ba9d6c83f31a344903
sergiorico3 <clubofsk8ers@hotmail.com>
2019-06-13 00:31:52 +0100

    Mutation Inversion update

--- src/ga/geneticOperators/MutationInversion.java
+++ src/ga/geneticOperators/MutationInversion.java
@@ -20,13 +20,13 @@ public class MutationInversion<I extends IntVectorIndividual, P extends Problem<
         int l = parent.length;
         for(int k = 0; k < 5; k++){//repeat process 5 times
             //get 2 random integers between 0 and size of array
-            int r1 = GeneticAlgorithm.random.nextInt(l-parent[0])-parent[0];
-            int r2 = GeneticAlgorithm.random.nextInt(l-parent[0])-parent[0];
+            int r1 = GeneticAlgorithm.random.nextInt(l);
+            int r2 = GeneticAlgorithm.random.nextInt(l);
             //to make sure the r1 < r2
             while(r1 >= r2)
             {
-                r1 = GeneticAlgorithm.random.nextInt(l-parent[0])-parent[0];
-                r2 = GeneticAlgorithm.random.nextInt(l-parent[0])-parent[0];
+                r1 = GeneticAlgorithm.random.nextInt(l);
+                r2 = GeneticAlgorithm.random.nextInt(l);
             }
             //this code inverts (i.e. reverses) elements between r1..r2 inclusive
             int mid = r1 + ((r2 + 1) - r1) / 2;
