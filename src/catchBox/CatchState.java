78b4e6373fa594dcb190e2a56a9daf5e284b0172
sergiorico3 <clubofsk8ers@hotmail.com>
2019-06-17 04:09:54 +0100

    getNumBox Atualizado

--- src/catchBox/CatchState.java
+++ src/catchBox/CatchState.java
@@ -112,8 +112,15 @@ public class CatchState extends State implements Cloneable {
     }
 
     public int getNumBox() {
-        //TODO
-        return 0;
+        int numBoxes=0;
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix.length; j++) {
+                if (matrix[i][j] == Properties.BOX){
+                    numBoxes++;
+                }
+            }
+        }
+        return numBoxes;
     }
 
     public void setCellCatch(int line, int column) {
